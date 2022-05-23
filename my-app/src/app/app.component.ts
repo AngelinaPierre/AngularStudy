31a9955c9efae673b1a052d194996594fad3fb92
AngelinaPierre <gabrielle.tripodi@hotmail.com>
2022-05-23 20:08:30 -0300

    angular tutorial - format with the uppercasePipe

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
+
+
+// The component class code, written in TypeScript.
